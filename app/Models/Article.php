f60c99eef09fe84139ef1cafc9e398b016e0d3bd
BlackPeace5 <4522105@gmail.com>
2022-10-18 20:55:49 +0300

    added README.md

---
+++ app/Models/Article.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $filltable = ['title', 'body', 'img', 'slug'];
+
+
+    public function comments() {
+        return $this->HasMany(Comment::class);
+    }
+
+    public function state() {
+        return $this->hasOne(State::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
+}
